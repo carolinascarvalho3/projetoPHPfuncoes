45bc81152a81555c23468a0d2ff4b1ad03a881ee
carolinascarvalho3 <carolsinha10@outlook.com>
2024-07-31 17:11:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Exercícios</title>
+</head>
+<body>
+   <button><a href="exercicio1.php">Exercício 01</a></button><br>
+   <button><a href="exercicio2.php">Exercício 02</a></button><br>
+   <button><a href="exercicio3.php">Exercício 03</a></button><br> 
+   <button><a href="exercicio4.php">Exercício 04</a></button><br>
+   <button><a href="exercicio5.php">Exercício 05</a></button><br>
+   <button><a href="exercicio6.php">Exercício 06</a></button><br>
+   <button><a href="exercicio7.php">Exercício 07</a></button><br>
+   <button><a href="exercicio8.php">Exercício 08</a></button><br>
+   <button><a href="exercicio9.php">Exercício 09</a></button><br>
+   <button><a href="exercicio10.php">Exercício 10</a></button><br>
+   <button><a href="exercicio11.php">Exercício 11</a></button><br>
+   <button><a href="exercicio13.php">Exercício 12</a></button><br>
+   <button><a href="exercicio14.php">Exercício 13</a></button><br>
+   <button><a href="exercicio15.php">Exercício 14</a></button><br>
+   <button><a href="exercicio16.php">Exercício 15</a></button><br>
+   <button><a href="exercicio17.php">Exercício 16</a></button><br>
+   <button><a href="exercicio18.php">Exercício 17</a></button><br>
+   <button><a href="exercicio19.php">Exercício 18</a></button><br>
+   <button><a href="exercicio20.php">Exercício 19</a></button><br>
+   <button><a href="exercicio21.php">Exercício 20</a></button><br>
+
+</body>
+</html>
